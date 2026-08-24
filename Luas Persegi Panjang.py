a8d56089d4afe3dac3de59dc89e540a77812fb2e
Gio Putra <buge31743@gmail.com>
2026-08-24 15:29:05 +0700

    Program FUNGSI

---
+++ Luas Persegi Panjang.py
@@ -0,0 +1,11 @@
+"""
+Program Menghitung Luas Persegi Panjang
+"""
+
+panjang = int(input("Masukkan panjang: "))
+lebar = int(input("Masukkan lebar: "))
+
+luas = panjang + lebar
+
+print(f"Luas persegi panjang dengan panjang {panjang} dan lebar {lebar} adalah {luas}")
+print(" ")
