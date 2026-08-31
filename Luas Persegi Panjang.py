8e8ef73ad429dea26bd7391ed81bf4b125adf547
GIO PUTRA FAJAR ANTONIO <buge31743@gmail.com>
2026-08-31 15:14:25 +0700

    Update Luas Persegi Panjang.py

--- Luas Persegi Panjang.py
+++ Luas Persegi Panjang.py
@@ -1,11 +1,24 @@
-"""
-Program Menghitung Luas Persegi Panjang
-"""
+# Fungsi segitiga
+def segitiga(alas, tinggi):
+    x = print("Luas segitiga:", 0.5 * alas * tinggi)
+    y = print("Keliling segitiga:", alas + tinggi + (alas**2 + tinggi**2)**0.5)
+    return x, y
 
-panjang = int(input("Masukkan panjang: "))
-lebar = int(input("Masukkan lebar: "))
+# Fungsi persegi
+def persegi(sisi):
+    x = print("Luas persegi:", sisi * sisi)
+    y = print("Keliling persegi:", 4 * sisi)
+    return x, y
 
-luas = panjang + lebar
+# Fungsi persegi panjang
+def persegi_panjang(panjang, lebar):
+    x = print("Luas persegi panjang:", panjang * lebar)
+    y = print("Keliling persegi panjang:", 2 * (panjang + lebar))
+    return x, y
 
-print(f"Luas persegi panjang dengan panjang {panjang} dan lebar {lebar} adalah {luas}")
-print(" ")
+# Fungsi lingkaran
+def lingkaran(jari_jari):
+    import math
+    x = print("Luas Lingkaran:", math.pi * jari_jari ** 2)
+    y = print("keliling Lingkaran:", 2 * math.pi * jari_jari)
+    return x, y
